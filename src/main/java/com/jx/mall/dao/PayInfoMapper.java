f7a20e00db95f0e5d93380738966f59732a1e389
LiuJingwei0420 <58125786+LiuJingwei0420@users.noreply.github.com>
2020-03-19 20:38:16 +0800

    payInfo

---
+++ src/main/java/com/jx/mall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.jx.mall.dao;
+
+import com.jx.mall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
