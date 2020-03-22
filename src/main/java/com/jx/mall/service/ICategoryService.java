0577842b6de92d3fff5f00f247c873a6a3f7b488
LiuJingwei0420 <58125786+LiuJingwei0420@users.noreply.github.com>
2020-03-22 20:33:05 +0800

    商品列表上

--- src/main/java/com/jx/mall/service/ICategoryService.java
+++ src/main/java/com/jx/mall/service/ICategoryService.java
@@ -4,8 +4,11 @@ import com.jx.mall.vo.CategoryVo;
 import com.jx.mall.vo.ResponseVo;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ICategoryService {
 
     ResponseVo<List<CategoryVo>> selectAll();
+
+    void findSubCategoryId(Integer id, Set<Integer> resultSet);
 }
--- src/main/java/com/jx/mall/service/impl/CategoryServieceImpl.java
+++ src/main/java/com/jx/mall/service/impl/CategoryServieceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.jx.mall.consts.MallConst.ROOT_PARENT_ID;
@@ -52,7 +53,24 @@ public class CategoryServieceImpl  implements ICategoryService {
 
 
     }
-    private void findSubCategory(List<CategoryVo> categoryVoList, List<Category> categories) {
+
+    @Override
+    public void findSubCategoryId(Integer id, Set<Integer> resultSet) {
+        List<Category> categories = categoryMapper.selectAll();
+        findSubCategoryId(id, resultSet, categories);
+    }
+
+    private void findSubCategoryId(Integer id, Set<Integer> resultSet, List<Category> categories) {
+        for (Category category : categories) {
+            if (category.getParentId().equals(id)) {
+                resultSet.add(category.getId());
+                findSubCategoryId(category.getId(),resultSet, categories);
+            }
+        }
+    }
+
+
+        private void findSubCategory(List<CategoryVo> categoryVoList, List<Category> categories) {
         for (CategoryVo categoryVo : categoryVoList) {
 
             List<CategoryVo> subCategoryVoList = new ArrayList<>();
